abddda6ecac934e6c160e0176f9e11ebf88fa60b
Abdullah Arif <130820980+DevelopbyAbdullah@users.noreply.github.com>
2025-10-07 11:51:50 +0500

    Delete new exceel web directory

--- new exceel web/find_headers.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel('NOVA - COST Pricing Template  - Master2.xlsx', header=None)
-
-for i in range(15, 25):
-    print(f'\nRow {i}:')
-    print(df.iloc[i, 0:10].tolist())
